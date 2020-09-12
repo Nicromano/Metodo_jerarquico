b7e5cb69691a47d053ab11f0749162238fa35d9d
Nicromano <40124504+Nicromano@users.noreply.github.com>
2020-09-12 15:36:54 -0500

    Grafico

--- Jerarquico.py
+++ Jerarquico.py
@@ -5,23 +5,27 @@ Created on Fri Sep 11 19:49:35 2020
 @author: NICROMANO
 """
 
-from Helpers import obtenerUltimoCluster, encontrarMenor
+from Helpers import  encontrarMenor, nVariablesCluster
 from Enlaces import enlacePromedio, enlaceCompleto, enlaceSimple
 
 def agruparCluster(matriz, enlace, original = None):
-    matriz_copia = matriz
-    menor, x, y = encontrarMenor(matriz)
-    matriz = matriz.drop([y], axis=1)
-    matriz = matriz.drop([y], axis=0)
-    matriz = matriz.rename(columns={x: "({},{})".format(x,y)})
-    matriz = matriz.rename(index={x: "({},{})".format(x,y)})
-    if enlace == 'simple':
-        matriz = enlaceSimple(matriz, matriz_copia, x, y)
-    if enlace == 'completo':
-        matriz = enlaceCompleto(matriz, matriz_copia, x, y)
-    if enlace == 'promedio':
-        matriz = enlacePromedio(matriz, matriz_copia, x, y, original)
-    return matriz, [x, y, menor]
+    cluster = []
+    copia = matriz
+    while(len(matriz)!=2):
+        matriz_copia = matriz
+        menor, x, y = encontrarMenor(matriz)
+        matriz = matriz.drop([y], axis=1)
+        matriz = matriz.drop([y], axis=0)
+        matriz = matriz.rename(columns={x: "({},{})".format(x,y)})
+        matriz = matriz.rename(index={x: "({},{})".format(x,y)})
+        if enlace == 'simple':
+            matriz = enlaceSimple(matriz, matriz_copia, x, y)
+        if enlace == 'completo':
+            matriz = enlaceCompleto(matriz, matriz_copia, x, y)
+        if enlace == 'promedio':
+            matriz = enlacePromedio(matriz, matriz_copia, x, y, original)
+        cluster.append([x, y, menor, nVariablesCluster(copia, x) + nVariablesCluster(copia, y)])
+    return matriz, cluster
 
 def obtenerDendrograma():
     pass
--- main.py
+++ main.py
@@ -12,26 +12,27 @@ from Jerarquico import agruparCluster
 from Helpers import transformarMatriz
 from scipy.cluster import hierarchy
 import numpy as np
+import plotly.figure_factory as ff
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     data = pd.read_csv('data.csv', sep=',', header=None)
     matriz_distancia = transformarMatriz(data) #calculaDistancia(data, 'euclidean')
     copia_matriz = matriz_distancia
-    cluster = []
-    while len(matriz_distancia) != 2:
-        matriz_distancia, x = agruparCluster(matriz_distancia, 'promedio', copia_matriz)
-        cluster.append(x)
+    matriz_result, cluster = agruparCluster(matriz_distancia, 'promedio', copia_matriz)
     print(cluster)
 
+    
     
     ytdist = np.array([662., 877., 255., 412., 996., 295., 468., 268., 400., 754., 564., 138., 219., 869., 669.])
     Z = hierarchy.linkage(ytdist, 'single')
     plt.figure()
+    print(type(Z))
     print(Z)
     dn = hierarchy.dendrogram(Z)
-    print(dn)
+   
     
+
     
     
     
